00d3cdcce15af33fcd574743520288d424ee820e
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2024-08-15 20:22:02 +0700

    Add files via upload

---
+++ Informasi Kendaraan/Mobil.java
@@ -0,0 +1,26 @@
+public class Mobil extends Kendaraan {
+    private long harga;
+    protected String namaClass = "Mobil";
+
+    protected void setHarga(String harga){
+        this.harga = Long.parseLong(harga);
+        harga = null;
+    }
+
+    protected long getHarga(){
+        return harga;
+    }
+
+    protected String keterangan(){
+        return (namaClass+" : Ini adalah class "+namaClass);
+    }
+
+    public String keterangan_kendaraan(){
+        return super.namaClass+" : "+super.keterangan();
+    }
+
+    protected void hapus(){
+        harga = 0;
+        super.hapus();
+    }
+}
